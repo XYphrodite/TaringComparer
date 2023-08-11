953ba947269a66543fd8eed3032422d8b66182c9
Maksim <mrmaksoracle89537089833@yandex.ru>
2023-08-11 18:53:34 +0300

    write something

--- TaringCompare/Services/TaringComparison.cs
+++ TaringCompare/Services/TaringComparison.cs
@@ -3,6 +3,7 @@ using LiveCharts.Defaults;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using TaringCompare.Models;
 
@@ -10,11 +11,72 @@ namespace TaringCompare.Services
 {
     public class TaringComparison
     {
-        public static ChartValues<ObservablePoint> Interpolize(ChartValues<ObservablePoint> points, int precision)
+        public static ChartValues<ObservablePoint> Interpolize(ChartValues<ObservablePoint> points, int precision = 100)
         {
+            points = new ChartValues<ObservablePoint>(points.OrderBy(p => p.X));
             ChartValues<ObservablePoint> toReturn = new();
-            throw new NotImplementedException();
+            int n = points.Count - 1;
+            double minX = points.Min(p => p.X), maxX = points.Max(p => p.X);
+            double h = (maxX - minX) / (precision - 1);
+
+
+            double[] a_arr = new double[n];
+            for (int i = 0; i < a_arr.Length; i++)
+            {
+                a_arr[i] = FindA(a_arr, i);
+            }
+
+
+            for (double i = minX; i < maxX; i += h)
+            {
+                var newPoint = new ObservablePoint { X = i };
+                double newY = FindY(a_arr, i, points.Select(p => p.X).ToArray());
+                newPoint.Y = newY;
+                toReturn.Add(newPoint);
+            }
+            return toReturn;
+
+
+
+
+
+
+
+
+
+            double FindY(double[] a_arr, double x, double[] x_arr)
+            {
+                double y = 0;
+                for (int i = 0; i < a_arr.Length; i++)
+                {
+                    y += a_arr[i] * FindXPolynom(x, x_arr, i);
+                }
+                return y;
+            }
+
+            double FindXPolynom(double x, double[] x_arr, int num)
+            {
+                double xPolynom = 1;
+                for (int i = 0; i < num - 1; i++)
+                {
+                    xPolynom *= (x - x_arr[i-1]);
+                }
+                return xPolynom;
+            }
+            double FindA(double[] a_arr, int i)
+            {
+
+                if (i == 0)
+                    return points[i].Y;
+                else
+                {
+                    double a = points[i].Y - a_arr[i - 1] * FindXPolynom(points[i].X, points.Select(p => p.X).ToArray(), i);
+                    return a;
+                }
+            }
         }
+
+
 
         public static double Compare(List<double> list1, List<double> list2)
         {
