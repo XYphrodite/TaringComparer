672cadc24be6491ab8912b85aa2b68296e6b2302
Maksim <mrmaksoracle89537089833@yandex.ru>
2023-08-15 18:31:14 +0300

    >fixes

--- TaringCompare/Services/TaringLoader.cs
+++ TaringCompare/Services/TaringLoader.cs
@@ -1,10 +1,7 @@
 ﻿using Microsoft.Win32;
-using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text.Json;
-using System.Threading.Tasks;
 using TaringCompare.Data;
 using TaringCompare.Models;
 
